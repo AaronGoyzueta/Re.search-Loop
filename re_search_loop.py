65f94e6c7ffb9f9654295ee14da06676566a84fa
Aaron Goyzueta <aarongoyzueta@gmail.com>
2020-02-21 20:25:07 -0500

    Added initial version of the function

--- re_search_loop.py
+++ re_search_loop.py
@@ -1,1 +1,27 @@
 # Function for re.search loop
+
+import re
+
+def main(exp, text):
+    cont = 0
+    output = re.search(exp, text)
+    if output:
+        
+        cont = True
+        lb = output.span()[0]
+        rb = output.span()[1]
+        
+        print(output.span())
+        
+        while cont:
+
+            new_output = re.search(exp, text[rb:])
+            if new_output:
+                lb += new_output.span()[1]
+                rb += new_output.span()[1]  
+                print((lb, rb))
+            else:
+                cont = False
+            
+    else:
+        print("None found")
